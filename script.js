a19b8ec469b97da1dd67ab3d03fcd8b80e034969
Royston Henson <rlqhenson@hotmail.com>
2017-02-24 18:40:57 +0000

    get data from github api

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   $(document).on('keypress', '#username', function(event) {
     if (event.which === 13) {
       var username = $(this).val();
-      console.log('username was : ' + username);
+      console.log('username is : ' + username);
+      getGithubInfo();
     }
   });
 });
+
 function getGithubInfo(username) {
   var url = 'https://api.github.com/users/' + username;
   var xml = new XMLHttpRequest();
