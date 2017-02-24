0dd6be1c336c50d30445ebbae9f925f856f41feb
Royston Henson <rlqhenson@hotmail.com>
2017-02-24 15:49:37 +0000

    add function to grab username from input box

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// github user finder example
+$(document).ready(function() {
+  $(document).on('keypress', '#username', function() {
+    if (event.which === 13) {
+      var username = $(this).val();
+      console.log('username was : ' + username);
+    }
+  });
+});
