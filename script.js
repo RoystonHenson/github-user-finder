094f061a3bacef62f8dc2062bc2e7b6dd9501cfa
Royston Henson <rlqhenson@hotmail.com>
2017-02-24 15:53:41 +0000

    add missing argument, add xml request

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $(document).on('keypress', '#username', function() {
+  $(document).on('keypress', '#username', function(event) {
     if (event.which === 13) {
       var username = $(this).val();
       console.log('username was : ' + username);
     }
   });
 });
+function getGithubInfo(username) {
+  var url = 'https://api.github.com/users/' + username;
+  var xml = new XMLHttpRequest();
+  xml.open('GET', url, false);
+  xml.send();
+
+  var data = xml.responseText;
+  console.log(data);
+}
